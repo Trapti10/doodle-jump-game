55773c584a7e706cc04bbd42f943635c077ac771
Trapti <traptivishwakarma2007@gmail.com>
2025-12-14 16:15:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const grid = document.querySelector(".grid")
+    const doodler = document.createElement('div')
+  let doodlerLeftSpace = 50
+    function createDoodler(){
+        grid.appendChild(doodler);
+        doodler.classList.add('doodler')
+        doodler.style.left =doodlerLeftSpace+'px'; 
+        doodler.style.bottom =doodlerLeftSpace+'px'; 
+
+    }
+    createDoodler()
+
+    // function strat(){
+
+    // }
+})
